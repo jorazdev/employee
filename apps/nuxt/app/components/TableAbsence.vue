74f38c519df14e7ee3507f6d47ca36b5572a232c
Joraz <jorazdev@gmail.com>
2026-02-22 12:29:17 +0300

    feat: mise à jour table absences + dashboard

--- apps/nuxt/app/components/TableAbsence.vue
+++ apps/nuxt/app/components/TableAbsence.vue
@@ -1,103 +1,109 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import type { TAbsence, TEmployee } from '~/types/absence'
-import FormAbsence from '~/components/FormAbsence.vue'
-import Modal from '~/components/Modal.vue'
-import useAbscence from '~/composables/absence'
-import BtnActionTable from '~/components/table/BtnActionTable.vue'
-import HeaderTable from '~/components/table/HeaderTable.vue'
-import BodyTable from '~/components/table/BodyTable.vue'
-import useTools from '~/composables/tools'
-import Button from '~/components/form/Button.vue'
+import { ref, computed } from "vue";
+import type { TAbsence, TEmployee } from "~/types/absence";
+import FormAbsence from "~/components/FormAbsence.vue";
+import Modal from "~/components/Modal.vue";
+import useAbscence from "~/composables/absence";
+import BtnActionTable from "~/components/table/BtnActionTable.vue";
+import HeaderTable from "~/components/table/HeaderTable.vue";
+import BodyTable from "~/components/table/BodyTable.vue";
+import useTools from "~/composables/tools";
+import Button from "~/components/form/Button.vue";
 
 type props = {
-  absences: TAbsence[]
-}
-const props = defineProps<props>()
+  absences: TAbsence[];
+};
+const props = defineProps<props>();
 
-const { absenceForm, onUpdateAbsence } = useAbscence()
-const { getUserRandom, formatDate } = useTools()
+const { absenceForm, onUpdateAbsence } = useAbscence();
+const { getUserRandom, formatDate } = useTools();
 
-const currentDate = ref(new Date())
-const show = ref<boolean>(false)
-const action = ref<'add'|'edit'>('add')
+const currentDate = ref(new Date());
+const show = ref<boolean>(false);
+const action = ref<"add" | "edit">("add");
 
 // Generates all dates for the current month
 const monthDates = computed(() => {
-  const year = currentDate.value.getFullYear()
-  const month = currentDate.value.getMonth()
-  const daysInMonth = new Date(year, month + 1, 0).getDate()
+  const year = currentDate.value.getFullYear();
+  const month = currentDate.value.getMonth();
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
 
-  const dates: string[] = []
+  const dates: string[] = [];
   for (let day = 1; day <= daysInMonth; day++) {
-    const d = new Date(year, month, day)
-    dates.push(d.toISOString().split('T')[0] || '') // ex: "2025-10-01"
+    const d = new Date(year, month, day);
+    dates.push(d.toISOString().split("T")[0] || ""); // ex: "2025-10-01"
   }
-  return dates
-})
+  return dates;
+});
 
 const employees = computed(() => {
-  const grouped = new Map<string, TEmployee>()
+  const grouped = new Map<string, TEmployee>();
   for (const a of props.absences) {
-    const key = `${a.firstName}-${a.lastName}`
+    const key = `${a.firstName}-${a.lastName}`;
     if (!grouped.has(key)) {
-      grouped.set(key, {abscenceId: a.abscenceId, firstName: a.firstName, lastName: a.lastName, personnelNumber: a.personnelNumber || '', absences: [] })
+      grouped.set(key, {
+        abscenceId: a.abscenceId,
+        firstName: a.firstName,
+        lastName: a.lastName,
+        personnelNumber: a.personnelNumber || "",
+        absences: [],
+      });
     }
-    grouped.get(key)!.absences.push({ begin: a.startDate, end: a.endDate })
+    grouped.get(key)!.absences.push({ begin: a.startDate, end: a.endDate });
   }
-  return Array.from(grouped.values())
-})
+  return Array.from(grouped.values());
+});
 
-const onSelect = async (emp: TEmployee|null) => {
-  if(emp != null){
-    action.value = 'edit'
-    absenceForm.value.abscenceId = emp.abscenceId
-    absenceForm.value.firstName = emp.firstName
-    absenceForm.value.lastName = emp.lastName
-    absenceForm.value.startDate = emp.absences[0]?.begin || ''
-    absenceForm.value.endDate = emp.absences[0]?.end || ''
-  }else {
-    action.value = 'add'
-    const data = await getUserRandom()
-    if(data && absenceForm ){
-      absenceForm.value.firstName = data.results[0]?.name.first ?? ""
-      absenceForm.value.lastName = data.results[0]?.name.last ?? ""
-      absenceForm.value.startDate = formatDate(new Date(), 'iso')
-      absenceForm.value.endDate = formatDate(new Date(), 'iso')
+const onSelect = async (emp: TEmployee | null) => {
+  if (emp != null) {
+    action.value = "edit";
+    absenceForm.value.abscenceId = emp.abscenceId;
+    absenceForm.value.firstName = emp.firstName;
+    absenceForm.value.lastName = emp.lastName;
+    absenceForm.value.startDate = emp.absences[0]?.begin || "";
+    absenceForm.value.endDate = emp.absences[0]?.end || "";
+  } else {
+    action.value = "add";
+    const data = await getUserRandom();
+    if (data && absenceForm) {
+      absenceForm.value.firstName = data.results[0]?.name.first ?? "";
+      absenceForm.value.lastName = data.results[0]?.name.last ?? "";
+      absenceForm.value.startDate = formatDate(new Date(), "iso");
+      absenceForm.value.endDate = formatDate(new Date(), "iso");
     }
   }
-  show.value = true
-}
-
+  show.value = true;
+};
 </script>
 
 <template>
-    <BtnActionTable v-model="currentDate"/>
-    <div class=" border border-gray-300 rounded-lg overflow-x-auto max-h-[400px] overflow-y-auto">
-        <table v-if="absences.length > 0" 
-            class="min-w-full border-collapse text-sm text-left text-gray-700">
-          <HeaderTable v-model="show" 
-            :monthDates/>
-          <BodyTable v-model="show" 
-            :employees 
-            :monthDates 
-            @onSelect="onSelect"/>
-        </table>
-    </div>
-    <Button icon="add" 
-      title="Add Absent"
-      className="bg-blue-400 text-white mt-5" 
-      @onclick="onSelect(null)"/>
-    
-    <Modal v-model="show" 
-      @onClose="show = false" 
-      class="z-30">
-      <FormAbsence v-model="absenceForm" 
-        :action
-        @onClose="show = false" 
-        @onUpdateAbsence="onUpdateAbsence"/>
-    </Modal>
-   
+  <BtnActionTable v-model="currentDate" />
+  <div
+    class="border border-gray-300 rounded-lg overflow-x-auto max-h-[400px] overflow-y-auto"
+  >
+    <table
+      v-if="absences.length > 0"
+      class="min-w-full border-collapse text-sm text-left text-gray-700"
+    >
+      <HeaderTable v-model="show" :monthDates />
+      <BodyTable v-model="show" :employees :monthDates @onSelect="onSelect" />
+    </table>
+  </div>
+  <Button
+    icon="add"
+    title="Add Absent"
+    className="bg-blue-400 text-white mt-5"
+    @onclick="onSelect(null)"
+  />
+
+  <Modal v-model="show" @onClose="show = false" class="z-30">
+    <FormAbsence
+      v-model="absenceForm"
+      :action
+      @onClose="show = false"
+      @onUpdateAbsence="onUpdateAbsence"
+    />
+  </Modal>
 </template>
 
 <style lang="css" scoped>
@@ -105,7 +111,8 @@ table {
   border-collapse: collapse;
   width: 100%;
 }
-th, td {
+th,
+td {
   text-align: center;
 }
 </style>
--- apps/nuxt/app/pages/dashboard.vue
+++ apps/nuxt/app/pages/dashboard.vue
@@ -1,53 +1,67 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import useAbscence from '~/composables/absence';
-import Button from '~/components/form/Button.vue'
-import type { TUser } from '~/types/user.type';
+import { onMounted } from "vue";
+import useAbscence from "~/composables/absence";
+import Button from "~/components/form/Button.vue";
+import type { TUser } from "~/types/user.type";
 
 definePageMeta({
-    middleware: ['auth']
-})
+  middleware: ["auth"],
+});
 
+const { absences } = useAbscence();
 
-const { absences } = useAbscence()
-
-const tokenCo = useCookie<string>('token')
-const userCo = useCookie<TUser>('user')
+const tokenCo = useCookie<string>("token");
+const userCo = useCookie<TUser>("user");
 
 const onLogout = () => {
-    tokenCo.value = ''
-    window.location.reload()
-}
+  tokenCo.value = "";
+  window.location.reload();
+};
 </script>
 
 <template>
-    <div class="flex flex-col items-center justify-center min-h-screen w-full p-4 bg-[#EAEAE0]/20">
-        <div class="w-full  max-w-7xl h-20 bg-[#2B2B2B] rounded-2xl mb-5 
-            flex items-center justify-between p-2">
-            <div class="flex items-center justify-center gap-2">
-                <div>
-                    <div v-if="!userCo" class="w-16 h-16 bg-slate-50 rounded-full border-2 border-black"></div>
-                    <img v-else :src="userCo.avatarUrl" alt="" class="w-16 h-16 rounded-full border-2 border-lime-400">
-                </div>
-                <div v-if="userCo">
-                    <h1 class="text-2xl text-white font-semibold">{{ userCo.firstName }}</h1>
-                    <h1 class="text-lg text-white font-semibold">{{ userCo.lastName }}</h1>
-                    <h1 class="text-[10px] text-lime-500 font-semibold">{{ userCo.email }}</h1>
-                </div>
-            </div>
-            <Button icon="logout" 
-                title="Sign out"
-                className="text-white" 
-                @onclick="onLogout()"/>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen w-full p-4 bg-[#EAEAE0]/20"
+  >
+    <div
+      class="w-full max-w-7xl h-20 bg-[#2B2B2B] rounded-2xl mb-5 flex items-center justify-between p-2"
+    >
+      <div class="flex items-center justify-center gap-2">
+        <div>
+          <div
+            v-if="!userCo"
+            class="w-16 h-16 bg-slate-50 rounded-full border-2 border-black"
+          ></div>
+          <img
+            v-else
+            :src="userCo.avatarUrl"
+            alt=""
+            class="w-16 h-16 rounded-full border-2 border-lime-400"
+          />
         </div>
-        <div class="w-full max-w-7xl bg-white rounded-lg shadow-lg p-6">
-            <TableAbsence v-if="absences && absences.length > 0" 
-                :absences/>
-
+        <div v-if="userCo">
+          <h1 class="text-2xl text-white font-semibold">
+            {{ userCo.firstName }}
+          </h1>
+          <h1 class="text-lg text-white font-semibold">
+            {{ userCo.lastName }}
+          </h1>
+          <h1 class="text-[10px] text-lime-500 font-semibold">
+            {{ userCo.email }}
+          </h1>
         </div>
+      </div>
+      <Button
+        icon="logout"
+        title="Sign out"
+        className="text-white"
+        @onclick="onLogout()"
+      />
     </div>
+    <div class="w-full max-w-7xl bg-white rounded-lg shadow-lg p-6">
+      <TableAbsence :absences />
+    </div>
+  </div>
 </template>
 
-<style lang="css" scoped>
-
-</style>
+<style lang="css" scoped></style>
